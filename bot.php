8736f1fcdb26a90228c944b1231eccd063a29a86
tanagorj <tanagorj@gmail.com>
2017-01-06 15:24:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,8 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-			if($text == 'จัดไป'){
+			$jsonObj = json_decode($text);
+			if($jsonObj == 'จัดไป'){
 				// Make a POST Request to Messaging API to reply to sender
 				$url = 'https://api.line.me/v2/bot/message/reply';
 				$data = [
