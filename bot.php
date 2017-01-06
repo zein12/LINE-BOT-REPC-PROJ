397eedc69b38cf5350cb80b25c8a647645f18509
tanagorj <tanagorj@gmail.com>
2017-01-06 14:22:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				//'text' => $text
+				'text' => "ทดลองอยู่"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
